5167abf89ca2c616fb8e8359f4378e66b8e0c96b
Bias.Xie <biasxie@BiasXies-MacBook-Air.local>
2018-04-18 17:18:05 +0800

    配置AppId和MasterKey

--- parse-server-example-master/index.js
+++ parse-server-example-master/index.js
@@ -15,10 +15,10 @@ if (!databaseUri) {
 
 //启动的函数
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  databaseURI: databaseUri || 'mongodb://localhost/test',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
-  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'AppId_2',
+  masterKey: process.env.MASTER_KEY || 'MasterKey_2', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
